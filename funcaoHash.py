67d2aa0f6456e903edbe44ffcd0c908cdb38059e
ludihan <65617704+ludihan@users.noreply.github.com>
2025-02-24 20:07:12 -0300

    format

--- funcaoHash.py
+++ funcaoHash.py
@@ -1,4 +1,3 @@
-
 import pandas as pd
 """
 Implementação Jenkins One-at-a-time Hash (https://en.wikipedia.org/wiki/Jenkins_hash_function)
@@ -6,33 +5,42 @@ Esta função é focada em bit mixing, com passos extras para gerar efeito avala
 Retornará um inteiro para cada palavra, o inteiro será de grande magnitude, possibilitado aplicar os módulos diversas vezes, optei por mesclar multiplicação por primo
 e módulo a base de potência de dois.
 """
+
+
 def jenkins_one_at_a_time(palavra: str) -> int:
     hash_value = 0
     for caractere in palavra:
-        hash_value += ord(caractere) # ord() retorna o valor ASCII de cada caractere, ou a gente tem que fazer isso manualmente também?
+        # ord() retorna o valor ASCII de cada caractere, ou a gente tem que fazer isso manualmente também?
+        hash_value += ord(caractere)
         hash_value += (hash_value << 10)
         hash_value ^= (hash_value >> 6)
-    
+
     hash_value += (hash_value << 3)
     hash_value ^= (hash_value >> 11)
     hash_value += (hash_value << 15)
 
     hash_value = (hash_value * 35969) % (2**32)
 
-    return hash_value & 0xffffffff # Essa função é orientada a 32 bits, isso vai limitar o valor do hash para 32 bits no Python, caso contrário, valores absurdamente grandes ocorrerão.
+    # Essa função é orientada a 32 bits, isso vai limitar o valor do hash para 32 bits no Python, caso contrário, valores absurdamente grandes ocorrerão.
+    return hash_value & 0xffffffff
 
-local_arq = "words.txt" # Caso queira testar, lembrar de colocar na mesma pasta do arquivo.
+
+# Caso queira testar, lembrar de colocar na mesma pasta do arquivo.
+local_arq = "words.txt"
 
 with open(local_arq, "r", encoding="utf-8") as arquivo:
     lista = pd.Series([linha.strip() for linha in arquivo])
 
-lista = lista.dropna().astype(str) # Tava dando erro na leitura, isso garante que qualquer valor nulo será ignorado e todos os valores de linha serão convertidos para string.
+# Tava dando erro na leitura, isso garante que qualquer valor nulo será ignorado e todos os valores de linha serão convertidos para string.
+lista = lista.dropna().astype(str)
 lista_hash = lista.apply(jenkins_one_at_a_time)
 
-m = lista_hash.nsmallest(100) # Controle, apenas para verificar os valores obtidos, se obteve algum valor extremamente baixo ou alto.
+# Controle, apenas para verificar os valores obtidos, se obteve algum valor extremamente baixo ou alto.
+m = lista_hash.nsmallest(100)
 # m = lista_hash.nlargest(100)
 for i in m:
     print(i)
 
-duplicatas = lista_hash.value_counts() # Verificando quantas possíveis colisões teríamos até aqui.
+# Verificando quantas possíveis colisões teríamos até aqui.
+duplicatas = lista_hash.value_counts()
 print(duplicatas[duplicatas > 1])
