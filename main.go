afc9d5d414dbd669dc344dd718d7e995c1e43a27
ludihan <65617704+ludihan@users.noreply.github.com>
2025-03-11 19:27:38 -0300

    progress

--- main.go
+++ main.go
@@ -1,40 +1,67 @@
 package main
 
 import (
-	"crypto/sha256"
+	"bufio"
 	_ "embed"
 	"fmt"
+	"math"
 	"strings"
 )
+
+func hash(word string) uint64 {
+	const fnvOffsetBasis uint64 = 14695981039346656037
+	const fnvPrime uint64 = 1099511628211
+
+	var hash uint64 = fnvOffsetBasis
+
+	for i := range len(word) {
+		hash *= fnvPrime
+		hash ^= uint64(word[i])
+	}
+
+	return hash
+}
 
 const pageSize = 5
 
 //go:embed words.txt
 var rawData string
 
-func main() {
-	// arquivo de db
-	data := []string{}
+type Database []string
 
-	todosBuckets := []Bucket{}
+type Bucket map[string]uint
 
-	for v := range strings.Lines(rawData) {
-		data = append(data, strings.Trim(v, "\n"))
-	}
+type HashIndex []Bucket
 
-	/*
-		for i := 0; i < len(data); i += pageSize {
-			fmt.Printf("%#v\n", data[i:pageSize+i])
+func (db Database) getPage(index uint) ([]string, bool) {
+	clamp := func(low, high uint) uint {
+		if low < high {
+			return low
+		} else {
+			return high
 		}
-	*/
-
-	for _, v := range data {
-		resultadoHash := sha256.Sum256(v)
-
 	}
+	if index*pageSize < uint(len(db)) {
+		return db[index*pageSize : clamp((index*pageSize)+pageSize, uint(len(db)))], true
+	}
+
+	return []string{}, false
 }
 
-type Bucket struct {
-	keys   []string
-	values []int
+func (db Database) pageCount() int {
+	return int(math.Ceil(float64(len(db)) / float64(pageSize)))
+}
+
+func main() {
+	// arquivo de db
+	file := bufio.NewScanner(strings.NewReader(rawData))
+	var db Database
+
+	for file.Scan() {
+		text := file.Text()
+		if len(strings.TrimSpace(text)) != 0 {
+			db = append(db, file.Text())
+		}
+	}
+
 }
