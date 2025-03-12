ed71da9df4fa057070c30c3b956cfd87626bdbd3
ludihan <65617704+ludihan@users.noreply.github.com>
2025-03-12 05:18:22 -0300

    add gui

--- src/main.go
+++ src/main.go
@@ -4,9 +4,16 @@ import (
 	"bufio"
 	_ "embed"
 	"fmt"
+	"image/color"
+	"log"
 	"math"
 	"os"
 	"strings"
+
+	"gioui.org/app"
+	"gioui.org/op"
+	"gioui.org/text"
+	"gioui.org/widget/material"
 )
 
 // https://wikipedia.org/wiki/Fowler–Noll–Vo_hash_function#FNV-1_hash
@@ -74,7 +81,7 @@ func main() {
 	}
 
 	// (FR)
-	bucketSize := 2
+	bucketSize := 5
 
 	// (NB)
 	bucketCount := (len(db.data) / bucketSize) + int(float64(len(db.data))*0.05)
@@ -91,54 +98,104 @@ func main() {
 		hashIndex[i].values = make(map[string]uint)
 	}
 
-	colisoes := 0
+	collisions := 0
+	overflows := 0
+	isOverflow := false
 	for i, v := range db.data {
 		hashed := hash(v)
 		bucket := &hashIndex[hashed%uint64(len(hashIndex))]
-		if len(bucket.values) > 0 {
-			colisoes++
-		}
-
 		for bucket.overflow != nil {
 			bucket = bucket.overflow
+			isOverflow = true
 		}
 
 		if len(bucket.values) >= int(bucketSize) {
+			overflows++
 			newBucket := &Bucket{values: make(map[string]uint)}
 			bucket.overflow = newBucket
 			bucket = newBucket
 		}
 
+		if isOverflow {
+			collisions++
+		}
 		bucket.values[v] = uint(i) / db.pageSize
 	}
 
-	fmt.Println("colisoes:   ", colisoes)
+	fmt.Println("collisions: ", collisions, float64(collisions)/float64(wordCount))
+	fmt.Println("overflows:  ", overflows, float64(overflows)/float64(wordCount))
 	fmt.Println("")
-	reader := bufio.NewReader(os.Stdin)
-	for {
-        fmt.Print(">>> ")
-		text, _ := reader.ReadString('\n')
+	/*
+		reader := bufio.NewReader(os.Stdin)
+		for {
+		    fmt.Print(">>> ")
+		    text, _ := reader.ReadString('\n')
+		    text = strings.TrimSpace(text)
+		    if len(text) == 0 {
+		        continue
+		    }
+		    if text[0] == ':' {
+		        num, err := strconv.Atoi(text[1:])
+		        if err == nil {
+		            if num >= 0 {
+		                fmt.Println(db.getPage(uint(num)))
+		            } else {
+		                fmt.Println("only positive")
+		            }
+		            continue
+		        }
+		    }
 
-		text = strings.TrimSpace(text)
-		found := false
-		page := uint(0)
-		bucket := &hashIndex[hash(text)%uint64(len(hashIndex))]
-		for bucket != nil {
-			fmt.Println(bucket)
-			for k, v := range bucket.values {
-				if k == text {
-					page = v
-					found = true
-				}
-			}
-			bucket = bucket.overflow
+		    found := false
+		    page := uint(0)
+		    bucket := &hashIndex[hash(text)%uint64(len(hashIndex))]
+		    for bucket != nil {
+		        fmt.Println(bucket)
+		        for k, v := range bucket.values {
+		            if k == text {
+		                page = v
+		                found = true
+		            }
+		        }
+		        bucket = bucket.overflow
+		    }
+		    if found {
+		        fmt.Println("Found in page", page)
+		        foundPage, _ := db.getPage(page)
+		        fmt.Println(foundPage, "\n")
+		    } else {
+		        fmt.Println("not found\n")
+		    }
 		}
-		if found {
-			fmt.Println("Found in page", page)
-			foundPage, _ := db.getPage(page)
-			fmt.Println(foundPage, "\n")
-		} else {
-			fmt.Println("not found\n")
+	*/
+
+	go func() {
+		window := new(app.Window)
+		err := run(window)
+		if err != nil {
+			log.Fatal(err)
+		}
+		os.Exit(0)
+	}()
+	app.Main()
+}
+
+func run(window *app.Window) error {
+	theme := material.NewTheme()
+	theme.Bg = color.NRGBA{0x1c, 0x1b, 0x22, 0xff}
+	var ops op.Ops
+	for {
+		switch e := window.Event().(type) {
+		case app.DestroyEvent:
+			return e.Err
+		case app.FrameEvent:
+			gtx := app.NewContext(&ops, e)
+			title := material.H3(theme, "CrabDB")
+			maroon := color.NRGBA{R: 128, G: 0, B: 0, A: 255}
+			title.Color = maroon
+			title.Alignment = text.Middle
+			title.Layout(gtx)
+			e.Frame(gtx.Ops)
 		}
 	}
 }
