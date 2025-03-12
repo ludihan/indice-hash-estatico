83687941908052cbd359ce0e75012c50d840310d
ludihan <65617704+ludihan@users.noreply.github.com>
2025-03-12 14:23:05 -0300

    overflow

--- src/main.go
+++ src/main.go
@@ -34,6 +34,25 @@ func hash(word string) uint64 {
 	return hash
 }
 
+func nextPrime(n int) int {
+	for !isPrime(n) {
+		n++
+	}
+	return n
+}
+
+func isPrime(n int) bool {
+	if n < 2 {
+		return false
+	}
+	for i := 2; i*i <= n; i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
 //go:embed words.txt
 var rawData string
 
@@ -67,31 +86,23 @@ type Bucket struct {
 	overflow *Bucket
 }
 
-func main() {
-	isCli := flag.Bool("cli", false, "Starts a simple cli instead of a gui.")
-	flag.Parse()
-	if *isCli {
-		cli()
-	} else {
-		go func() {
-			window := new(app.Window)
-			window.Option(app.Title("Hash Index"))
-			if err := gui(window); err != nil {
-				log.Fatal(err)
-			}
-			os.Exit(0)
-		}()
-		app.Main()
-	}
+type App struct {
+	db          Database
+	bucketSize  uint
+	bucketCount uint
+	wordCount   uint
+	collisions  uint
+	overflows   uint
+	hashIndex   []Bucket
 }
 
-func cli() {
+func reindex(pageSize uint) App {
 	// db file
 	file := bufio.NewScanner(strings.NewReader(rawData))
 	db := Database{
 		data: []string{},
 		// user input
-		pageSize: 5,
+		pageSize: pageSize,
 	}
 
 	for file.Scan() {
@@ -105,7 +116,7 @@ func cli() {
 	bucketSize := 5
 
 	// (NB)
-	bucketCount := (len(db.data) / bucketSize) + int(float64(len(db.data))*0.05)
+    bucketCount := nextPrime(int(math.Ceil(float64(len(db.data)) / float64(bucketSize)) + float64(len(db.data)) * 0.1 ))
 
 	// (NR)
 	wordCount := len(db.data)
@@ -146,7 +157,59 @@ func cli() {
 	fmt.Println("collisions: ", collisions, float64(collisions)/float64(wordCount))
 	fmt.Println("overflows:  ", overflows, float64(overflows)/float64(wordCount))
 	fmt.Println("")
+
+	return App{
+		db:          db,
+		bucketSize:  uint(bucketSize),
+		bucketCount: uint(bucketCount),
+		wordCount:   uint(wordCount),
+		collisions:  uint(collisions),
+		overflows:   uint(overflows),
+		hashIndex:   hashIndex,
+	}
+}
+
+func main() {
+	isCli := flag.Bool("cli", false, "Starts a simple cli instead of a gui.")
+	flag.Parse()
+	if *isCli {
+		cli()
+	} else {
+		go func() {
+			window := new(app.Window)
+			window.Option(app.Title("Hash Index"))
+			if err := gui(window); err != nil {
+				log.Fatal(err)
+			}
+			os.Exit(0)
+		}()
+		app.Main()
+	}
+}
+
+func cli() {
 	reader := bufio.NewReader(os.Stdin)
+	var pageSize uint
+	for {
+		fmt.Print("page size:   ")
+		text, _ := reader.ReadString('\n')
+		text = strings.TrimSpace(text)
+		size, err := strconv.ParseUint(text, 10, 0)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		if size == 0 {
+			fmt.Println("cant use 0")
+			continue
+		}
+		pageSize = uint(size)
+		break
+	}
+
+	app := reindex(pageSize)
+	db := app.db
+	hashIndex := app.hashIndex
 	for {
 		fmt.Print(">>> ")
 		text, _ := reader.ReadString('\n')
@@ -193,67 +256,6 @@ func cli() {
 }
 
 func gui(window *app.Window) error {
-	// db file
-	file := bufio.NewScanner(strings.NewReader(rawData))
-	db := Database{
-		data: []string{},
-		// user input
-		pageSize: 5,
-	}
-
-	for file.Scan() {
-		text := file.Text()
-		if len(strings.TrimSpace(text)) != 0 {
-			db.data = append(db.data, file.Text())
-		}
-	}
-
-	// (FR)
-	bucketSize := 5
-
-	// (NB)
-	bucketCount := (len(db.data) / bucketSize) + int(float64(len(db.data))*0.05)
-
-	// (NR)
-	wordCount := len(db.data)
-
-	fmt.Println("bucketSize: ", bucketSize)
-	fmt.Println("bucketCount:", bucketCount)
-	fmt.Println("wordCount:  ", wordCount)
-
-	hashIndex := make([]Bucket, bucketCount)
-	for i := range hashIndex {
-		hashIndex[i].values = make(map[string]uint)
-	}
-
-	collisions := 0
-	overflows := 0
-	isOverflow := false
-	for i, v := range db.data {
-		hashed := hash(v)
-		bucket := &hashIndex[hashed%uint64(len(hashIndex))]
-		for bucket.overflow != nil {
-			bucket = bucket.overflow
-			isOverflow = true
-		}
-
-		if len(bucket.values) >= int(bucketSize) {
-			overflows++
-			newBucket := &Bucket{values: make(map[string]uint)}
-			bucket.overflow = newBucket
-			bucket = newBucket
-		}
-
-		if isOverflow {
-			collisions++
-		}
-		bucket.values[v] = uint(i) / db.pageSize
-	}
-
-	fmt.Println("collisions: ", collisions, float64(collisions)/float64(wordCount))
-	fmt.Println("overflows:  ", overflows, float64(overflows)/float64(wordCount))
-	fmt.Println("")
-
 	// GUI stuff
 	var ops op.Ops
 
@@ -268,8 +270,21 @@ func gui(window *app.Window) error {
 
 			layout.Flex{
 				Axis:    layout.Vertical,
-				Spacing: layout.SpaceStart,
+				Spacing: layout.SpaceEnd,
 			}.Layout(gtx,
+				layout.Rigid(
+					func(gtx layout.Context) layout.Dimensions {
+						margins := layout.UniformInset(unit.Dp(10))
+
+						return margins.Layout(gtx,
+							func(gtx layout.Context) layout.Dimensions {
+								title := material.H1(theme, "huhhhhh")
+								return title.Layout(gtx)
+							},
+						)
+
+					},
+				),
 				layout.Rigid(
 					func(gtx layout.Context) layout.Dimensions {
 						margins := layout.UniformInset(unit.Dp(10))
